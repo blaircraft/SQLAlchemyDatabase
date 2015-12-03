b5437e98d2f378e8b48ce64201103d78ba02cf7d
Blair Craft <blair@blaircraft.net>
2015-12-03 08:41:59 -0800

    Added URL to setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     description='Abstraction classes for SQLAlchemy connections.',
     author='Blair Craft',
     author_email='blair@blaircraft.net',
+    url='https://github.com/blaircraft/SQLAlchemyDatabase',
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
