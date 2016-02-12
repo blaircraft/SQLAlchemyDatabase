e3e0b9e1994ccd8ecb693de9bf8d21bc957bdf91
Blair Craft <blair@blaircraft.net>
2016-02-11 17:15:36 -0800

    Updated to version 1.01

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sqlalchemydatabase',
-    version='1.0',
+    version='1.01',
     description='Abstraction classes for SQLAlchemy connections.',
     author='Blair Craft',
     author_email='blair@blaircraft.net',
